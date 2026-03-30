34e862185d32e4b6f52f8d59352fad88cd3fd43b
RosdasHH <164037153+RosdasHH@users.noreply.github.com>
2026-03-30 19:57:36 +0200

    Ui

--- lib/filaments.dart
+++ lib/filaments.dart
@@ -34,52 +34,68 @@ class _FilamentTabState extends State<FilamentTab> {
     } else {
       return Scaffold(
         appBar: AppBar(title: Text("Filaments")),
-        body: ListView(
-          children: [
-            for (Spool filament in filaments!)
-              Card(
-                child: InkWell(
-                  onTap: () async {
-                    await Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        settings: const RouteSettings(name: "filamentdata"),
-                        builder: (context) => FilamentSettings(spool: filament),
-                      ),
-                    );
-                    setState(() {
-                      filaments = null;
-                    });
-                    getFilaments();
-                  },
-                  child: ListTile(
-                    title: Text(filament.slicerFilamentName),
-                    minLeadingWidth: 20,
-                    leading: SizedBox(
-                      width: 20,
-                      child: Center(
-                        child: SizedBox.square(
-                          dimension: 20,
-                          child: DecoratedBox(
-                            decoration: BoxDecoration(
-                              color: toFlutterColor(filament.rgba),
-                              shape: BoxShape.circle,
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: ListView(
+            children: [
+              for (Spool filament in filaments!)
+                Card(
+                  child: InkWell(
+                    onTap: () async {
+                      await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          settings: const RouteSettings(name: "filamentdata"),
+                          builder: (context) =>
+                              FilamentSettings(spool: filament),
+                        ),
+                      );
+                      setState(() {
+                        filaments = null;
+                      });
+                      getFilaments();
+                    },
+                    child: ListTile(
+                      title: Text(filament.slicerFilamentName),
+                      minLeadingWidth: 20,
+                      leading: SizedBox(
+                        width: 20,
+                        child: Center(
+                          child: SizedBox.square(
+                            dimension: 20,
+                            child: DecoratedBox(
+                              decoration: BoxDecoration(
+                                color: toFlutterColor(filament.rgba),
+                                shape: BoxShape.circle,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    subtitle: Text(filament.colorName),
-                    trailing: CircularProgressIndicator(
-                      value:
-                          ((filament.labelWeight - filament.weightUsed) /
-                                  filament.labelWeight)
-                              .toDouble(),
+                      subtitle: Text(filament.colorName),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          if (filament.qrcode != null)
+                            Padding(
+                              padding: EdgeInsets.only(right: 20),
+                              child: Icon(Icons.qr_code_2_rounded),
+                            )
+                          else
+                            SizedBox.shrink(),
+                          CircularProgressIndicator(
+                            value:
+                                ((filament.labelWeight - filament.weightUsed) /
+                                        filament.labelWeight)
+                                    .toDouble(),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-          ],
+            ],
+          ),
         ),
       );
     }
